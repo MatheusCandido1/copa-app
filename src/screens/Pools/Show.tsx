a3c54ab967ca739ab352f2a897d52340fbe040a8
Matheus Carvalho <matheus12bh@gmail.com>
2022-11-05 13:50:24 -0700

    add-find

--- src/screens/Pools/Show.tsx
+++ src/screens/Pools/Show.tsx
@@ -1,10 +1,103 @@
-import { Heading, VStack } from "native-base";
+import { useState } from "react";
+import { Heading, Text, VStack, useToast, Box} from "native-base";
+import { Keyboard } from 'react-native'
 
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
+import { api } from "../../services/api";
+import { useNavigation } from "@react-navigation/native";
+
 export function Show() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [code, setCode] = useState("");
+
+  const toast = useToast();
+  const { navigate } = useNavigation();
+
+  async function handleJoinPool() {
+    if(!code.trim()) {
+      return toast.show({
+        render: () => {
+          return (
+            <Box bg="red.500" p={5} rounded={5}>
+              <Text color="white">O codígo do bolão é obrigatório</Text>
+            </Box>
+          )
+        },
+        placement: 'top',
+        duration: 3000,
+      })
+    }
+
+
+
+    try {
+      setIsLoading(true);
+
+      await api.post('/pools/join', {code});
+      Keyboard.dismiss()
+      setCode('')
+
+      toast.show({
+        render: () => {
+          return (
+            <Box bg="green.500" p={5} rounded={5}>
+              <Text color="white">Bem vindo ao bolão!</Text>
+            </Box>
+          )
+        },
+        placement: 'top',
+        duration: 3000,
+      })
+
+      navigate('listPools')
+    } catch (error) {
+      console.log(error);
+      if(error.response?.data?.message === 'Pool not found') {
+        return toast.show({
+          render: () => {
+            return (
+              <Box bg="red.500" p={5} rounded={5}>
+                <Text color="white">Bolão não encontrado</Text>
+              </Box>
+            )
+          },
+          placement: 'top',
+          duration: 3000,
+        })
+      }
+
+      if(error.response?.data?.message === 'Already joined') {
+        return toast.show({
+          render: () => {
+            return (
+              <Box bg="red.500" p={5} rounded={5}>
+                <Text color="white">Você já está nesse bolão</Text>
+              </Box>
+            )
+          },
+          placement: 'top',
+          duration: 3000,
+        })
+      }
+
+      toast.show({
+        render: () => {
+          return (
+            <Box bg="red.500" p={5} rounded={5}>
+              <Text color="white">Erro ao buscar bolão</Text>
+            </Box>
+          )
+        },
+        placement: 'top',
+        duration: 3000,
+      })
+      setIsLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header showBackButton title="Buscar por código" />
@@ -18,9 +111,12 @@ export function Show() {
         <Input
           mb={8}
           placeholder="Qual o código do bolão?"
+          onChangeText={setCode}
+          value={code}
+          autoCapitalize="characters"
         />
 
-        <Button title="BUSCAR BOLÃO" />
+        <Button title="BUSCAR BOLÃO" onPress={handleJoinPool} />
       </VStack>
     </VStack>
   );
